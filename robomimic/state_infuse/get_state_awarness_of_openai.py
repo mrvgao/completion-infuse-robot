961904f43273b9cd9a78e2d9af4c5963a18755ac
mrvgao <minchiuan@zju.edu.cn>
2024-10-07 19:55:24 -0400

    change prompts

--- robomimic/state_infuse/get_state_awarness_of_openai.py
+++ robomimic/state_infuse/get_state_awarness_of_openai.py
@@ -5,10 +5,12 @@ import numpy as np
 
 api_key = "sk-..."
 
+
 # Function to encode the image
 def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
 
 def change_ndarray_to_base64(image):
     # Convert the image to base64
@@ -24,47 +26,52 @@ def get_internal_state_form_openai(image_left, image_hand, image_right, step, ho
     base64_image_right = change_ndarray_to_base64(image_right)
 
     headers = {
-      "Content-Type": "application/json",
-      "Authorization": f"Bearer {api_key}"
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
     }
 
     payload = {
-      "model": "gpt-4o-mini",
-      "messages": [
-        {
-          "role": "user",
-          "content": [
+        "model": "gpt-4o-mini",
+        "messages": [
             {
-              "type": "text",
-              "text": open('configs/task_description.txt', 'r').read().replace('\n', '').format({'task': task, 'step': step, 'horizon': horizon})
-            },
-            {
-              "type": "image_url",
-              "image_url": {
-                "url": f"data:image/jpeg;base64,{base64_image_left}",
-                "detail": "high"
-              }
-            },
-            {
-                  "type": "image_url",
-                  "image_url": {
-                      "url": f"data:image/jpeg;base64,{base64_image_hand}",
-                      "detail": "high"
-                  }
-              },
-              {
-                  "type": "image_url",
-                  "image_url": {
-                      "url": f"data:image/jpeg;base64,{base64_image_right}",
-                      "detail": "high"
-                  }
-              }
-          ]
-        }
-      ],
-      "max_tokens": 300
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": open('configs/task_description.txt', 'r').read().replace('\n', '').format(
+                            {'task': task, 'step': step, 'horizon': horizon})
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image_left}",
+                            "detail": "high"
+                        }
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image_hand}",
+                            "detail": "high"
+                        }
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image_right}",
+                            "detail": "high"
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 300,
+        "temperature": 0.1,
+        "top_p": 1.0,
     }
 
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
-    print(response.json())
+    import pdb; pdb.set_trace()
+
+    return response.json()['choices'][0]['message']['content']
