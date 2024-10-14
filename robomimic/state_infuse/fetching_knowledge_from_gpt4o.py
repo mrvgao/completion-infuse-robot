90af0d009a7a1ffc686f90daf8d8532dbd5fc70c
mrvgao <minchiuan@zju.edu.cn>
2024-10-14 19:33:16 -0400

    add 2 infuse knowledge config with size 8

--- robomimic/state_infuse/fetching_knowledge_from_gpt4o.py
+++ robomimic/state_infuse/fetching_knowledge_from_gpt4o.py
@@ -52,12 +52,10 @@ def format_image(image):
     return image
 
 
-def extract_and_export_image(all_demo_dataset):
-    task_progress_states_mapping = {
-        # (task, progress) : state
-    }
+def collect_task_data(all_demo_dataset):
+    task_data = []
 
-    errors_recoding = {}
+    saved_keys = set()
 
     if hasattr(all_demo_dataset, 'datasets'):
         all_demo_dataset = all_demo_dataset
@@ -66,7 +64,6 @@ def extract_and_export_image(all_demo_dataset):
 
     for di, demo_dataset in enumerate(all_demo_dataset):
         exporting_dataset = demo_dataset
-
         eye_names = ['robot0_agentview_left_image', 'robot0_eye_in_hand_image', 'robot0_agentview_right_image']
 
         print('PROCESSING... dataset index with: ', di)
@@ -79,58 +76,91 @@ def extract_and_export_image(all_demo_dataset):
             # start at offset index if not padding for frame stacking
             demo_index_offset = 0 if exporting_dataset.pad_frame_stack else (exporting_dataset.n_frame_stack - 1)
             index_in_demo = i - demo_start_index + demo_index_offset
-
             complete_rate = round(index_in_demo / demo_length, 2)
-
             task_description = exporting_dataset._demo_id_to_demo_lang_str[demo_id]
 
             save_key = (task_description, complete_rate)
 
-            if save_key not in task_progress_states_mapping:
+            if save_key not in saved_keys:
                 left_image = exporting_dataset[i]['obs'][eye_names[0]][0]
                 hand_image = exporting_dataset[i]['obs'][eye_names[1]][0]
                 right_image = exporting_dataset[i]['obs'][eye_names[2]][0]
 
-                print(f'getting task {task_description} in progress {complete_rate} from openai')
+                print(f'Collecting task {task_description} in progress {complete_rate} for openai')
 
-                left_image = format_image(left_image)
-                hand_image = format_image(hand_image)
-                right_image = format_image(right_image)
+                task_data.append({
+                    "save_key": save_key,
+                    "task_description": task_description,
+                    "complete_rate": complete_rate,
+                    "left_image": format_image(left_image),
+                    "hand_image": format_image(hand_image),
+                    "right_image": format_image(right_image)
+                })
 
-                try:
-                    s = time.time()
-                    internal_state = get_internal_state_form_openai(
-                        left_image, hand_image,
-                        right_image,
-                        complete_rate,
-                        task_description,
-                        with_complete_rate=True,
-                        write_image=False,
-                        with_image_format_change=False
-                    )
+                saved_keys.add(save_key)
+            else:
+                # Already processed, skip this task
+                continue
 
-                    internal_state = re.sub(r'[\n\t]+', '', internal_state)
-                    internal_state = internal_state.replace('python', '')
-                    internal_state = internal_state.strip('`')
+    return task_data
 
-                    internal_state = ast.literal_eval(internal_state)
 
-                    task_progress_states_mapping[save_key] = internal_state
-                except Exception as e:
-                    print('get error: ', e)
-                    print('when processing task: ', task_description, ' with progress: ', complete_rate)
+def process_task(task, task_progress_states_mapping):
+    save_key = task['save_key']
+    task_description = task['task_description']
+    complete_rate = task['complete_rate']
+    left_image = task['left_image']
+    hand_image = task['hand_image']
+    right_image = task['right_image']
 
-                    errors_recoding[(task_description, complete_rate)] = e
+    if save_key not in task_progress_states_mapping:
+        try:
+            print(f'Getting task {task_description} in progress {complete_rate} from openai')
+            s = time.time()
 
-            save_dir = os.path.join('state_db', task_description.replace(' ', '_').replace('/', '_'))
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir)
+            # Call the function to get internal state
+            internal_state = get_internal_state_form_openai(
+                left_image, hand_image, right_image,
+                complete_rate, task_description,
+                with_complete_rate=True,
+                write_image=False,
+                with_image_format_change=False
+            )
 
-            with open(os.path.join(save_dir, 'task_progress_states_mapping.pkl'), 'wb') as f:
-                pickle.dump(task_progress_states_mapping, f)
+            # Clean the result
+            internal_state = re.sub(r'[\n\t]+', '', internal_state)
+            internal_state = internal_state.replace('python', '')
+            internal_state = internal_state.strip('`')
+            internal_state = ast.literal_eval(internal_state)
 
-            with open(os.path.join(save_dir, 'error_recoding.pkl'), 'wb') as f:
-                pickle.dump(errors_recoding, f)
+        except Exception as e:
+            print('get error: ', e)
+            print('when processing task: ', task_description, ' with progress: ', complete_rate)
+
+    # Save the results to disk
+    save_dir = os.path.join('state_db', task_description.replace(' ', '_').replace('/', '_'))
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    with open(os.path.join(save_dir, 'task_progress_states_mapping.pkl'), 'wb') as f:
+        pickle.dump(task_progress_states_mapping, f)
+
+
+def extract_and_export_image_parallel(all_demo_dataset):
+    # Initialize containers for task progress and error recordings
+    task_progress_states_mapping = {}
+
+    # Collect all tasks into a container and update task_progress_states_mapping
+    task_data, errors_recoding = collect_task_data(all_demo_dataset)
+
+    # Process tasks in parallel
+    with concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count()) as executor:
+        futures = [executor.submit(process_task, task) for task in task_data]
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()  # Check for any exceptions raised during execution
+            except Exception as exc:
+                print(f'Task generated an exception: {exc}')
 
 
 def generate_concated_images_from_demo_path(task_name=None, file_path=None):
