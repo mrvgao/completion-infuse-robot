fe765d59a9455dcdecf0c57095ea242b59b4b900
mrvgao <minchiuan@zju.edu.cn>
2024-10-14 19:46:06 -0400

    add 2 infuse knowledge config with size 8

--- robomimic/state_infuse/fetching_knowledge_from_gpt4o.py
+++ robomimic/state_infuse/fetching_knowledge_from_gpt4o.py
@@ -143,9 +143,6 @@ def process_task(task):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-    with open(os.path.join(save_dir, 'task_progress_states_mapping.pkl'), 'wb') as f:
-        pickle.dump(task_progress_states_mapping, f)
-
 
 def extract_and_export_image_parallel(all_demo_dataset):
     # Initialize containers for task progress and error recordings
