d9a22695f767ff81629bc5b0dabffa04e5835cd0
mrvgao <minchiuan@zju.edu.cn>
2024-10-22 15:56:05 -0400

    add rollout for infuse 8

--- robomimic/utils/train_utils.py
+++ robomimic/utils/train_utils.py
@@ -368,7 +368,7 @@ def run_rollout(
                 # complete_rate = step_i / horizon
                 import pdb; pdb.set_trace()
 
-                complete_rate = complete_rate_by_model.cpu().detach().numpy()
+                complete_rate = complete_rate_by_model.cpu().detach().numpy()[0][0]
 
                 # complete_rate = complete_rate[0][0].cpu().detach().numpy()
 
